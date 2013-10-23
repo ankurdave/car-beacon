aec7199be30204a0304da8680fca0e5015b56d70
Ankur Dave <ankurdave@gmail.com>
2013-10-22 17:18:03 -0700

    Notify hard-coded number on low battery

---
+++ src/com/ankurdave/carbeacon/BatteryNotifier.java
@@ -0,0 +1,13 @@
+package com.ankurdave.carbeacon;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.telephony.SmsManager;
+
+public class BatteryNotifier extends BroadcastReceiver {
+    private static final String TAG = "BatteryNotifier";
+    public void onReceive(Context context, Intent intent) {
+      SmsManager.getDefault().sendTextMessage("5556", null, "Low battery", null, null);
+    }
+}
